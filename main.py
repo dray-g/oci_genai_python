7d964cd24d0c02d0ad8ccf03756d1bdc5f1aafbb
dray-g <166137099+dray-g@users.noreply.github.com>
2026-05-25 15:51:41 +0530

    test

--- main.py
+++ main.py
@@ -9,7 +9,7 @@ from dotenv import load_dotenv
 load_dotenv()
 
 genai.configure(api_key=os.getenv("GEMINI_API_KEY"))
-model = genai.GenerativeModel("gemini-1.5-flash")
+model = genai.GenerativeModel("gemini-2.5-flash")
 
 app = FastAPI(title="Gemini Chatbot")
 
